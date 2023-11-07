0705275a392d22494c7a02bd4b4268960bd901fe
mbraje <121793051+mbraje@users.noreply.github.com>
2023-11-07 21:08:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,13 +12,11 @@ const image = require("./controllers/image");
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized : false },
-    host: process.env.DATABASE_HOST, 
+    host : '127.0.0.1',
+    user : 'postgres',
     port: 5432,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PW,
-    database: process.env.DATABASE_DB
+    password : 'test',
+    database : 'smart-brain'
   }
 });
 
@@ -27,14 +25,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-app.get("/", (req, res)=> { res.send(database.users) })
+app.get("/", (req, res)=> { res.send("it is working") })
 app.post("/signin", signin.handleSignin(db, bcrypt))
 app.post("/register", (req, res) => { register.handleRegister(req, res, db ,bcrypt)})
 app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put("/image", (req, res) => { image.handleImage(req, res, db)})
 
 
-app.listen(3000, ()=> {
-	console.log("app is running on port 3000");
+app.listen(process.env.PORT || 3000, ()=> {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
-
